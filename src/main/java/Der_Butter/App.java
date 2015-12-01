e16d8d4fd805a0e95673e467c832018ea1e79c03
Michael Weber <m.weber@hs-furtwangen.de>
2015-12-01 16:36:21 +0100

    Resident management system implemented Unit tests for the resident management system implemented integrated cobertura

--- src/main/java/Der_Butter/App.java
+++ src/main/java/Der_Butter/App.java
@@ -1,17 +1,24 @@
 package Der_Butter;
 
-import java.io.*;
 import java.util.Scanner;
 
 
 
 /**
  * Hello world!
- *
+ * This class is the class in which we do all the things
+ * 
  */
 
 public class App 
 {
+	/**
+	 * Hello world!
+	 * this is the main method, which does stuff
+	 * 
+	 * @param args args contain the mainmethod paarameters
+	 * 
+	 */
 	public static void main(String[] args)
 	{
 		String input;
